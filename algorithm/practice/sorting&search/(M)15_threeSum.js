ed52c21fa9c00327c94505536085ee424bd3b0dd
Tr.Nghia <t.nghia2112279@gmail.com>
2026-05-07 21:03:41 +0700

    feat(algo): Three Sum solution - 1st practice attempt

---
+++ algorithm/practice/sorting&search/(M)15_threeSum.js
@@ -0,0 +1,31 @@
+var threeSum = function (nums) {
+  let result = [];
+  nums.sort((a, b) => a - b);
+  let left = 0;
+  let right = nums.length - 1;
+
+  for (let i = 0; i <= nums.length - 1; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    left = i + 1;
+
+    while (left < right) {
+      let sum = nums[i] + nums[left] + nums[right];
+
+      if (sum === 0) {
+        result.push([nums[i], nums[left], nums[right]]);
+
+        // while (left < right && nums[left] === nums[left + 1]) left++;
+        // while (left < right && nums[right] === nums[right - 1]) right--;
+
+        left++;
+        right--;
+      } else if (sum < 0) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+  return result;
+};
