19ce73b9c62b674449f7f6712ae210951eecd356
Tr.Nghia <t.nghia2112279@gmail.com>
2026-05-08 21:01:50 +0700

    refactor(algo): convert all Java files to JavaScript

---
+++ algorithm/practice/binary/(E)67_addBinary.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function (a, b) {
+  let result = "";
+  let carry = 0;
+  let i = a.length - 1;
+  let j = b.length - 1;
+
+  while (carry !== 0 || i >= 0 || j >= 0) {
+    let sum = carry;
+
+    if (i >= 0) {
+      sum += a.charCodeAt(i) - 48;
+      i--;
+    }
+
+    if (j >= 0) {
+      sum += b.charCodeAt(j) - 48;
+      j--;
+    }
+
+    result = (sum % 2) + result;
+    carry = Math.floor(sum / 2);
+  }
+
+  return result;
+};
