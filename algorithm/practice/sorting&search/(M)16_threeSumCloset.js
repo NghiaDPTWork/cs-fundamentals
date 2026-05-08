37a231a00a261f161e44a2431bb4868edb829153
Tr.Nghia <t.nghia2112279@gmail.com>
2026-05-08 20:20:50 +0700

    feat(algo): 3Sum Closest - 1st practice attempt

--- algorithm/practice/sorting&search/(M)16_threeSumCloset.js
+++ algorithm/practice/sorting&search/(M)16_threeSumCloset.js
@@ -4,31 +4,35 @@
  * @return {number}
  */
 var threeSumClosest = function (nums, target) {
-  let result = Infinity;
   let sum = 0;
   let left = 0;
   let right = 0;
 
   nums.sort((a, b) => a - b);
-  for (let i = 0; i <= nums.length - 2; i++) {
-    if (i > 0 && nums[i] === nums[i - 1]) continue;
+  let closest = nums[0] + nums[1] + nums[2];
 
+  if (closest > target) return closest;
+
+  for (let i = 1; i <= nums.length - 3; i++) {
     left = i + 1;
     right = nums.length - 1;
 
     while (left < right) {
       sum = nums[i] + nums[left] + nums[right];
+      if (sum === target) return target;
 
-      if (sum === target) {
-        return target;
-      } else if (sum < target) {
-        if (Math.abs(target - result) > Math.abs(target - sum)) result = sum;
+      if (Math.abs(sum - target) < Math.abs(closest - target)) {
+        closest = sum;
+      }
+
+      if (sum < target) {
         left++;
+        while (left < right && nums[left] === nums[left - 1]) left++;
       } else {
-        if (Math.abs(target - result) > Math.abs(target - sum)) result = sum;
         right--;
+        while (left < right && nums[right] === nums[right + 1]) right--;
       }
     }
   }
-  return result;
+  return closest;
 };
