5adb7d73ca742b5ce33f937673856c3fda75645d
Tr.Nghia <t.nghia2112279@gmail.com>
2026-05-09 21:08:20 +0700

    perf(alo): optmize woSumusing Hshgit aMap O(n) time [Java]

--- algorithm/practice/array/twoSum.java
+++ algorithm/practice/array/twoSum.java
@@ -1,12 +1,17 @@
+import java.util.HashMap;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i = 0; i <= nums.length - 2; i++){
-            for(int j = i + 1; j <= nums.length - 1; j++){
-                if(nums[i] + nums[j] == target){
-                    return new int[] {i, j};
-                }
+       HashMap<Integer, Integer> myMap = new HashMap<>();
+       int expect = 0;
+
+       for(int i = 0; i <= nums.length - 1; i++){
+            expect = target - nums[i];
+            if(myMap.containsKey(expect)){
+                return new int[] {myMap.get(expect), i};
             }
-        }
-        return new int[] {};
+            myMap.put(nums[i], i);
+       }
+       return new int[] {};
     }
 }
