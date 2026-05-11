348d4e494d139b76a9b7640c9dfe6e12cc1dad0a
Tr.Nghia <t.nghia2112279@gmail.com>
2026-05-11 10:42:55 +0700

    refactor(algo): majorityElement - minimalist approach using Sorting and Middle Eleme[Java]

--- algorithm/practice/array/majorityElement.java
+++ algorithm/practice/array/majorityElement.java
@@ -1,24 +1,8 @@
+import java.util.Arrays;
+
 class Solution {
     public int majorityElement(int[] nums) {
-        int count = 0;
-        int candidate = 0;
-
-        if (nums.length <= 1) {
-            return (nums.length == 1) ? nums[0] : -1;
-        }
-
-        for(int value: nums){
-            if (count == 0) {
-                candidate = value;
-            }
-
-            if (value == candidate) {
-                count++;
-            } else {
-                count--;
-            }
-        }
-
-        return candidate;
+       Arrays.sort(nums);
+       return nums[nums.length / 2];
     }
 }
