6a56bf5ab425990f4510b2e8e4753dea3545b8cb
Tr.Nghia <t.nghia2112279@gmail.com>
2026-05-11 10:06:32 +0700

    perf(algo): optimize groupAnagrams using Char Counting Key instead of Sort O(N*K) [Java]

--- algorithm/practice/array/groupAnagrams.java
+++ algorithm/practice/array/groupAnagrams.java
@@ -1,20 +1,35 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) return new ArrayList<>();
+
         HashMap<String, List<String>> myMap = new HashMap<>();
-        char[] formatChar; 
-        String format = "";
+        int[] count = new int[26];
+        StringBuilder sb = new StringBuilder();
+        String key = "";
 
+        for(String str: strs){
+            count = new int[26];
+            for(char character: str.toCharArray()){
+                count[character - 'a']++;
+            }
 
-        for(int i = 0; i <= strs.length - 1; i++){
-            formatChar = strs[i].toCharArray(); 
-            Arrays.sort(formatChar);
-            format = String.valueOf(formatChar);
+            sb = new StringBuilder();
+            for(int value: count){
+                sb.append('#');
+                sb.append(value);
+            }
+            key = sb.toString();
 
-            myMap.putIfAbsent(format, new ArrayList<>());
-
-            myMap.get(format).add(strs[i]);
+            if(!myMap.containsKey(key)){
+                myMap.put(key, new ArrayList<>());
+            }
+            
+            myMap.get(key).add(str);
         }
-
         return new ArrayList<>(myMap.values());
     }
 }
